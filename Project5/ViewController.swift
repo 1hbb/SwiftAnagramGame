8e43f0d289bc561b1d86e5954aa65b56c34aa549
Harun Burak Bursa <1fobofobi@gmail.com>
2021-02-01 00:41:21 +0300

    Initial Commit

---
+++ Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by burk burs on 1.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
